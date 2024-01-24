c4e37b32aec325ed6aa12947a5db6001b9a18712
Caleb Ilabija <calebilabija@gmail.com>
2024-01-24 06:14:28 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +16,47 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Check DataBase Connection
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dbconn', function() {
+    return view('dbconn');
 });
+
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+//Show create form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+// Manage Listings
+Route::get('/listings/manage', [ListingController::class, 'manage'])->middleware('auth');
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show register form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+// Create new Users
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Login User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
